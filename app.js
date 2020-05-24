d42ede15b6ea3e666821d051da396514251180f3
Aashir Sohail <asohial.bscs16seecs@seecs.edu.pk>
2020-05-25 00:58:56 +0500

    added basic json db

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+//var data = require('./responses.json');
+
+// Declare variables
+var fs = require('fs'),
+    obj
+
+// Read the file and send to the callback
+fs.readFile('./responses.json', handleFile)
+
+// Write the callback function
+function handleFile(err, data) {
+    if (err) throw err
+    obj = JSON.parse(data)
+    // You can now play with your datas
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,8 +33,8 @@ router.get('/', function(req, res) {
     res.json({ message: 'welcome to our ADAM AI api!' });
 });
 
-router.get('/hi', function(req, res) {
-    res.json({ hi: 'Hello' });
+router.get('/:id', function(req, res) {
+    res.send(obj);
 });
 
 app.use('/api', router);
